8914529f00591cd286c221d8ee854f24a193e40c
damiano <damiano.derin@gmail.com>
2018-12-10 18:43:46 +0100

    setup update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telegram_essential',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/damiano9669/telegram_essential.git',
+    license='',
+    author='damiano derin',
+    author_email='damiano.derin@gmail.com',
+    description='It is an essential library for telegram bots.'
+)
